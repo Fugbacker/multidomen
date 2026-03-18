d43c7e26f0d338bc617cdf6ceea80a9b1f00ebae
Jhonny Fugbucker <evgenzolotoff@gmail.com>
2026-03-18 11:00:09 +0400

    fix

--- services/ssr/getSiteProps.js
+++ services/ssr/getSiteProps.js
@@ -2,7 +2,9 @@ export function getSiteProps(context) {
   const protocol = context.req.headers['x-forwarded-proto'] || 'http'
   const rootHost = context.req.headers.host
   const site = context.req.headers['x-site'] || 'nspdm'
-
+  console.log('protocol', protocol)
+  console.log('rootHost', rootHost)
+  console.log('site', site)
   const host = rootHost && rootHost.split('.').slice(-2).join('.')
   const url = `${protocol}://${host}${context.req.url}`
 
